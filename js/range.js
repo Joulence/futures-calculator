5c3f6a8edb8478e602827feef1289aa1935b33b0
Joulence <you@example.com>
2022-09-04 14:25:47 +0300

    fixed slider

--- js/range.js
+++ js/range.js
@@ -6,9 +6,5 @@ rangeSlider.addEventListener("input", showSliderValue, false);
 function showSliderValue() {
   rangeBullet.innerHTML = rangeSlider.value;
   var bulletPosition = (rangeSlider.value /rangeSlider.max);
-<<<<<<< HEAD
   rangeBullet.style.left = (bulletPosition * 88) + "%";
-=======
-  rangeBullet.style.left = (bulletPosition * 75) + "%";
->>>>>>> e2e751d40363457b39f7beeea870c8c433d793fe
 }
